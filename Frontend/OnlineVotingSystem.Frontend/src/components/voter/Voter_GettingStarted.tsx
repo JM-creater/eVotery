df809769a11a67fb2b5bb606b49b31050642a78b
Joseph Martin Garado <garadojosephmartin98@gmail.com>
2024-04-17 10:59:28 +0800

    Changes in Voter_GettingStarted.tsx

--- Frontend/OnlineVotingSystem.Frontend/src/components/voter/Voter_GettingStarted.tsx
+++ Frontend/OnlineVotingSystem.Frontend/src/components/voter/Voter_GettingStarted.tsx
@@ -1,19 +1,49 @@
 import { Button, Flex, Result } from "antd"
 import Voter_ElectionPage from "./Voter_ElectionPage";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const GET_USERBYID_URL = 'https://localhost:7196/User/get-by-id/';
+
+type UserType = {
+    id?: string;
+    isVoted?: boolean;
+}
 
 const Voter_GettingStarted: React.FC = () => {
 
     const [selectedItemMenu, setSelectedItemMenu] = useState<string>('1');
+    const [user, setUser] = useState<UserType | null>(null);
 
     const handleGetStartedClick = () => {
         setSelectedItemMenu("2");
     };
 
+    useEffect(() => {
+        const id = localStorage.getItem('userId');
+
+        const fetchUser = async () => {
+            try {
+                const response = await axios.get(`${GET_USERBYID_URL}${id}`);
+                setUser(response.data);
+
+                if (response.data.isVoted) {
+                    setSelectedItemMenu("2");
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        if (id) {
+            fetchUser();
+        }
+    }, []);
+
     return (
         <React.Fragment>
             {
-                selectedItemMenu === "1" ? (
+                selectedItemMenu === "1" && !user?.isVoted ? (
                     <Flex justify="center" align="center">
                         <Result
                             status="success"
